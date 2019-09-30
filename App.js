9f839c559512d3117ea8d3ef039fac8c3bef4549
radin-reth <radin.reth@gmail.com>
2019-09-30 23:30:26 +0700

    testing ActionSheet

--- App.js
+++ App.js
@@ -9,6 +9,7 @@
 import React from 'react'
 import 
   { 
+    Root,
     Container,
     Header, 
     Left,
@@ -20,35 +21,56 @@ import
     Content,
     Text,
     Footer,
-    FooterTab
+    FooterTab,
+    ActionSheet
   }
   from 'native-base'
 
+let BUTTONS = ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"];
+let DESTRUCTIVE_INDEX = 3;
+let CANCEL_INDEX = 4;
+
 const App = () =>  {
   return (
-    <Container>
-      <Header>
-        <Left>
-          <Button transparent>
-            <Icon name="menu" />
+    <Root>
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name="menu" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Header</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content padder>
+          <Button onPress={() => 
+            ActionSheet.show(
+                {
+                  options: BUTTONS,
+                  cancelButtonIndex: CANCEL_INDEX,
+                  destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                  title: "Testing ActionSheet"
+                },
+                buttonIndex => {
+                  console.log(buttonIndex)
+                }
+              )
+          }>
+            <Text>Actions</Text>
           </Button>
-        </Left>
-        <Body>
-          <Title>Header</Title>
-        </Body>
-        <Right />
-      </Header>
-      <Content padder>
-        <Text>Hello World</Text>
-      </Content>
-      <Footer>
-        <FooterTab>
-          <Button full>
-            <Text>Footer</Text>
-          </Button>
-        </FooterTab>
-      </Footer>
-    </Container>
+        </Content>
+        <Footer>
+          <FooterTab>
+            <Button full>
+              <Text>Footer</Text>
+            </Button>
+          </FooterTab>
+        </Footer>
+      </Container>
+    </Root>
   )
 }
 
