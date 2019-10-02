e861714d8cce2cf9e3fabffed28bfcff9dce1e7e
radin-reth <radin.reth@gmail.com>
2019-10-02 22:50:12 +0700

    list all debtors

--- App.js
+++ App.js
@@ -15,6 +15,8 @@ import
     Left,
     Button,
     Icon,
+    ListItem,
+    List,
     Body,
     Title,
     Right,
@@ -22,56 +24,72 @@ import
     Text,
     Footer,
     FooterTab,
-    ActionSheet
   }
   from 'native-base'
 
-let BUTTONS = ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"];
-let DESTRUCTIVE_INDEX = 3;
-let CANCEL_INDEX = 4;
+class App extends React.Component  {
+  
+  render() {
+    const data = {
+      name: 'sambo',
+      debtors: [
+        {
+          id: 1,
+          name: 'sitha'
+        },
+        {
+          id: 2,
+          name: 'chenda'
+        },
+        {
+          id: 3,
+          name: 'vorleak'
+        }
+      ]
+    }
 
-const App = () =>  {
-  return (
-    <Root>
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name="menu" />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Header</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content padder>
-          <Button onPress={() => 
-            ActionSheet.show(
-                {
-                  options: BUTTONS,
-                  cancelButtonIndex: CANCEL_INDEX,
-                  destructiveButtonIndex: DESTRUCTIVE_INDEX,
-                  title: "Testing ActionSheet"
-                },
-                buttonIndex => {
-                  console.log(buttonIndex)
-                }
-              )
-          }>
-            <Text>Actions</Text>
-          </Button>
-        </Content>
-        <Footer>
-          <FooterTab>
-            <Button full>
-              <Text>Footer</Text>
-            </Button>
-          </FooterTab>
-        </Footer>
-      </Container>
-    </Root>
-  )
+    return (
+      <Root>
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent>
+                <Icon name="arrow-back" />
+              </Button>
+            </Left>
+            <Body>
+              <Title>All debtors</Title>
+            </Body>
+            <Right>
+              <Button transparent>
+                <Icon name="menu" />
+              </Button>
+            </Right>
+          </Header>
+
+          <Content padder>
+            <List>
+              {
+                data.debtors.map( d => {
+                  return <ListItem key={String(d.id)}>
+                    <Text>{d.name}</Text>
+                  </ListItem>
+                })
+              }
+            </List>
+          </Content>
+
+          <Footer>
+            <FooterTab>
+              <Button full>
+                <Text>Footer</Text>
+              </Button>
+            </FooterTab>
+          </Footer>
+        </Container>
+      </Root>
+    )
+  }
 }
 
 export default App;
